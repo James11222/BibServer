c1c3c2e4600f1b2e5218a72623e831a7b202c1a1
James <jamessunseri@berkeley.edu>
2022-04-12 11:16:36 -0700

    attempted testing

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from venv import create
 from flask import Flask, render_template, flash, request, redirect, url_for, abort, \
     send_from_directory
 from werkzeug.utils import secure_filename
@@ -6,18 +7,28 @@ from flask_sqlalchemy import SQLAlchemy
 from pybtex.database import parse_file
 import pandas as pd
 
-app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 4096 * 4096
-app.config['UPLOAD_EXTENSIONS'] = ['.bib']
-app.config['UPLOAD_PATH'] = 'uploads' 
-app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///collections.sqlite3'
-app.config['SECRET_KEY'] = "random string"
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['MAX_CONTENT_LENGTH'] = 4096 * 4096
+    app.config['UPLOAD_EXTENSIONS'] = ['.bib']
+    app.config['UPLOAD_PATH'] = 'uploads' 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///collections.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = "random string"
+
+    db.init_app(app)
+
+    return app
+
+app = create_app()
 
 # ------------------------------------------------------------
 #                       File Uploading
 # ------------------------------------------------------------
 
-db = SQLAlchemy(app)
+
 
 class Collections(db.Model):
    id = db.Column('collection_id', db.Integer, primary_key = True)
@@ -40,32 +51,12 @@ class Collections(db.Model):
         self.authors = authors
         self.ref_tag = ref_tag
 
-@app.template_filter('add_ext')
-def add_extension(s):
-    return s + '.bib'
-
-@app.route('/')
-def index():
-    files = os.listdir(app.config['UPLOAD_PATH'])
-    files = [os.path.splitext(file)[0] for file in files]
-    return render_template('index.html', files=files)
-
-@app.route('/', methods=['POST'])
-def upload_files():
-    uploaded_file = request.files['file']
-    filename = secure_filename(request.form.get("fname") + '.bib')
-    if filename != '':
-        file_ext = os.path.splitext(filename)[1]
-        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
-            abort(400)
-        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-
-        #add data to sql database:
-
-        bib_data = parse_file(os.path.join(app.config['UPLOAD_PATH'], filename))
-
-
-        for entry in bib_data.entries.keys():
+def generate_collection(bib_data):
+    """
+    Helper function to generate a collection from a BibTeX file.
+    """
+    
+    for entry in bib_data.entries.keys():
             #Make sure the data is well formatted
             try:
                  title_val = bib_data.entries[entry].fields["title"][1:-1]
@@ -105,6 +96,8 @@ def upload_files():
             except:
                 author_val= "NONE"
 
+                
+
 
             collection = Collections(request.form.get("fname"), 
                                      title_val,
@@ -118,6 +111,32 @@ def upload_files():
             db.session.add(collection)
             db.session.commit()
 
+@app.template_filter('add_ext')
+def add_extension(s):
+    return s + '.bib'
+
+@app.route('/')
+def index():
+    files = os.listdir(app.config['UPLOAD_PATH'])
+    files = [os.path.splitext(file)[0] for file in files]
+    if "__init__" in files:
+        files.remove("__init__")
+    return render_template('index.html', files=files)
+
+@app.route('/', methods=['POST'])
+def upload_files():
+    uploaded_file = request.files['file']
+    filename = secure_filename(request.form.get("fname") + '.bib')
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+            abort(400)
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+
+        #add data to sql database:
+        bib_data = parse_file(os.path.join(app.config['UPLOAD_PATH'], filename))
+        generate_collection(bib_data)
+
     return redirect(url_for('index'))
 
 @app.route('/uploads/<filename>')
@@ -128,31 +147,29 @@ def upload(filename):
 def show_all():
     return render_template('show_all.html', collections=Collections.query.all())
 
+
 # ------------------------------------------------------------
 
 # ------------------------------------------------------------
 #                       Querying
 # ------------------------------------------------------------
-
-@app.route('/query', methods=["GET", "POST"])
-def query():
-    query_string = str(request.form.get("fname"))
+def generate_results(query_string, con):
     if query_string == "None":
         results = ["Please enter a query to get results."]
     else:
         sql_cmd = "SELECT * FROM Collections WHERE " + str(query_string)
         try:
-            df = pd.read_sql_query(sql_cmd, con=db.engine)
+            df = pd.read_sql_query(sql_cmd, con)
             results = []
             for index, row in df.iterrows():
                 result_string =  ("Collection Name: " + row['collection_name'] + "\n" + 
-                                  "Title: " + row['title'] + "\n" +
-                                  "Year: " + str(row['year']) + "\n" +
-                                  "Volume: " + str(row['volume']) + "\n" +
-                                  "Journal: " + row['journal'] + "\n" + 
-                                  "Pages: " + row['pages'] + "\n" +
-                                  "Authors List: " + row['authors'] + "\n" + 
-                                  "Reference Tag: " + row['ref_tag'])
+                                    "Title: " + row['title'] + "\n" +
+                                    "Year: " + str(row['year']) + "\n" +
+                                    "Volume: " + str(row['volume']) + "\n" +
+                                    "Journal: " + row['journal'] + "\n" + 
+                                    "Pages: " + row['pages'] + "\n" +
+                                    "Authors List: " + row['authors'] + "\n" + 
+                                    "Reference Tag: " + row['ref_tag'])
 
                 result_string = result_string.replace('\n', '<br>')
                 results.append(result_string)
@@ -160,10 +177,16 @@ def query():
         except:
             results = ["Your query was '" + query_string + "'. There are no results that match that query, please try something else."]
 
-        
+    return results
+
+
+@app.route('/query', methods=["GET", "POST"])
+def query():
+    query_string = str(request.form.get("fname"))
+    results = generate_results(query_string=query_string, con=db.engine)
     return render_template('query.html', results=results)
 
-
 if __name__ == '__main__':
-    db.create_all()
-    app.run(host="0.0.0.0", port=5000, debug = True)
+    with app.app_context():
+        db.create_all()
+    app.run(host="0.0.0.0", port=5000)
---
+++ tests/tests.py
@@ -0,0 +1,38 @@
+from BibServer.app import generate_collection, add_extension, Collections
+from flask_sqlalchemy import SQLAlchemy
+from BibServer.app import create_app
+import pytest
+
+db = SQLAlchemy()
+
+def test_add_extension():
+	assert add_extension("test") == "test.bib"
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here (What does this even mean?)
+
+    yield app
+
+    # clean up / reset resources here (What does this even mean?)
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
+
+def test_show_example(client):
+    response = client.get("/query")
+    assert b"Available Column names are" in response.data
+
+	
+
